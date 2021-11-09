26c9f8e94512e4d26c581ecc8536423c91577b79
Amos Sunmbo Alagbe <sunboy005@gmail.com>
2021-11-09 18:48:05 +0100

    File IO Read Method

--- FileIO/Program.cs
+++ FileIO/Program.cs
@@ -47,19 +47,47 @@ namespace FileIO
             //    Console.WriteLine("Hello World!");
             //}
             string path = @"C:\Users\HP\Desktop\trial.txt";
-            //Console.WriteLine(File.ReadAllText(path);
+            Console.WriteLine("===============Read All Bytes==============");
+
+
+            Console.WriteLine(File.ReadAllText(path));
+           
+            Console.WriteLine("===============Read All Lines==============");
+
             string[] newData = File.ReadAllLines(path);
             for (int i = 0; i < newData.Length; i++)
             {
                 Console.WriteLine($"Number {i} item is {newData[i]}");
             }
 
-            Console.WriteLine("===================================="  );
+            Console.WriteLine("===============Read All Bytes=============="  );
             byte[] newData2 = File.ReadAllBytes(path);
             for (int i = 0; i < newData2.Length; i++)
             {
-                Console.WriteLine($"Number {i} item is {newData[i]}");
+                Console.WriteLine($"Number {i} item is {newData2[i]}");
             }
+
+            Console.WriteLine("=============== Writing using File IO ==============");
+            Console.WriteLine("=============== Writing All Text ==============");
+
+
+            string path1 = @"C:\Users\HP\Desktop\TestWriteAll.txt";
+            string path2 = @"C:\Users\HP\Desktop\TestWriteLine.txt";
+            string data1 = "I am writing using WriteAllData";
+            string data1Append = "It is awesome to use";
+            string[] data2 = { "Can u see the wonders of WriteLine", "it Only works with array" };
+            string[] data2Append = { "Am using AppendLine to add extra string to the  Array.", "This is Awesome" };
+            
+            File.WriteAllText(path1, data1);
+            File.WriteAllLines(path2, data2);
+
+            Console.WriteLine("=============== Writing All Text ==============");
+
+            File.AppendAllText(path1, data1Append);
+            File.AppendAllLines(path2, data2Append);
+
+
+
 
 
 
